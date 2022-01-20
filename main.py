79690dbb2dc9e88d49502c8570f6032682f35018
Jure Vidmar <vidmarjur@gmail.com>
2022-01-20 19:39:40 +0100

    Use QHBoxLayout to combine Label+Graph (ugly)

--- main.py
+++ main.py
@@ -4,17 +4,44 @@ from PyQt6.QtWidgets import (
     QApplication,
     QMainWindow,
     QWidget,
+    QLabel,
+    QDockWidget,
+    QListWidget,
+    QHBoxLayout
 )
+import pyqtgraph as pg
+import numpy as np
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        # UI initialization
+        # Widgets
+        self.widget = QWidget()     # Container widget
+        
+        self.labelWidget = QLabel("Hello World!")
+
+        self.plotWidget = pg.PlotWidget()
+        self.plotWidget.setXRange(0, 20, padding=0)
+        self.plotWidget.setYRange(0, 10, padding=0)
+        
+
+        self.layout = QHBoxLayout()
+        self.layout.addWidget(self.labelWidget)
+        self.layout.addWidget(self.plotWidget)
+        self.layout.setContentsMargins(10,10,30,30)
+        self.widget.setLayout(self.layout)
+        
         self.setWindowTitle("My plotter")
-        self.resize(800, 600)
+        self.resize(1000, 600)
+        self.setMinimumSize(600, 350)
         self.center()
-        # self.setCentralWidget()
+        self.setCentralWidget(self.widget)
+
+        # x = np.random.normal(size=100)
+        # y = np.random.normal(size=100)
+        # self.plotWidget.plot(x, y, pen=None, symbol='o')
+
 
     def center(self):
         qr = self.frameGeometry()
@@ -30,7 +57,7 @@ class MainWindow(QMainWindow):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-
+    
     window = MainWindow()
     window.show()
     app.exec()
